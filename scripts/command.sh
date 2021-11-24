89ec6c48b09d913a0ebe462166f1b54a956d87a2
Raoul de Heer <raouldeheer@users.noreply.github.com>
2021-11-24 21:55:47 +0100

    Added asd-update

--- scripts/command.sh
+++ scripts/command.sh
@@ -5,27 +5,44 @@ downloadAndRun() {
     echo "Finished $1"
 }
 
-for option in "$@"
-do
-    case $option in
-        docker)
-            downloadAndRun "docker" "979e472a38375e9dc9b1383039ce3127"
-        ;;
-        
-        ufw)
-            downloadAndRun "ufw" "ebd8cb22fdeb3b899c5599551821859a"
-        ;;
-        
-        setup)
-            downloadAndRun "setup" "699991ad6ad3284cb6908aa07d148561"
-        ;;
+if (( $# == 0 ))
+then
+    echo "No input!"
+    echo ""
+    echo "ASD - Advanced Script Downloader"
+    echo "Options: "
+    echo "docker"
+    echo "ufw"
+    echo "setup"
+    echo "update"
+    echo "asd-update"
+else
+    for option in "$@"
+    do
+        case $option in
+            docker)
+                downloadAndRun "docker" "979e472a38375e9dc9b1383039ce3127"
+            ;;
 
-        update)
-            wget -O - -q --show-progress https://github.com/raouldeheer/ASD/releases/latest/download/install.sh | bash
-        ;;
-        
-        *)
-            echo "unknown option"
-        ;;
-    esac
-done
+            ufw)
+                downloadAndRun "ufw" "ebd8cb22fdeb3b899c5599551821859a"
+            ;;
+
+            setup)
+                downloadAndRun "LXC setup" "699991ad6ad3284cb6908aa07d148561"
+            ;;
+
+            update)
+                downloadAndRun "LXC update" "9bae094b585c92c9e69ac7647b08825a"
+            ;;
+
+            asd-update)
+                wget -O - -q --show-progress https://github.com/raouldeheer/ASD/releases/latest/download/install.sh | bash
+            ;;
+
+            *)
+                echo "unknown option"
+            ;;
+        esac
+    done
+fi
